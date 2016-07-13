0f5aeff023464536a98606b5ea2fe6dd4b6f8b46
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-07-13 17:54:00 +0100

    add day 4 (1 and 2)

---
+++ day4/day4-1.rb
@@ -0,0 +1,15 @@
+require 'digest/md5'
+
+class Miner
+
+  def mine(secret)
+    result = ""
+    i = 0
+    loop do
+      i+=1
+      result = Digest::MD5.hexdigest(secret + i.to_s)
+      break if result[0..4] == "00000"
+    end
+    i
+  end
+end
