75f41da404e1e75861c7be7453417699eab67f09
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-07-14 17:46:29 +0100

    add generic solution with number of zeros

--- day4/day4-1.rb
+++ day4/day4-1.rb
@@ -1,14 +1,14 @@
 require 'digest/md5'
 
 class Miner
-
+  ZEROS = 5
   def mine(secret)
     result = ""
     i = 0
     loop do
       i+=1
       result = Digest::MD5.hexdigest(secret + i.to_s)
-      break if result[0..4] == "00000"
+      break if result[0..ZEROS-1] == "0"*ZEROS.to_s
     end
     i
   end
--- day4/day4-2.rb
+++ day4/day4-2.rb
@@ -2,13 +2,15 @@ require 'digest/md5'
 
 class Miner
 
+  ZEROS = 6
+
   def mine(secret)
     result = ""
     i = 0
     loop do
       i+=1
       result = Digest::MD5.hexdigest(secret + i.to_s)
-      break if result[0..5] == "000000"
+      break if result[0..ZEROS-1] == "0"*ZEROS
     end
     i
   end
