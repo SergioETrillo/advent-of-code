1c4bbbadfaf28ec2fefebc005894ac133791f5ab
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-07-14 18:38:34 +0100

    day 5 parts 1 and 2, and add .gitignore
    ignore .txt files

--- day5/day5-1.rb
+++ day5/day5-1.rb
@@ -8,6 +8,8 @@ class CheckNiceWords
   NOT_CD = /^((?!cd).)*$/
   NOT_PQ = /^((?!pq).)*$/
   NOT_XY = /^((?!xy).)*$/
+  PAIR_TWICE = /([a-z])([a-z]).*?(?:\1\2)/
+  BOB = /(.).\1/
 
   attr_reader :nice_words
 
@@ -15,9 +17,16 @@ class CheckNiceWords
     @words = get_raw_data()
   end
 
-  def get_nice_words
-    count_nice_words
+  def get_nice_words(mode="basic")
+    case mode
+    when "basic"
+      count_nice_words
+    when "improved"
+      count_improved_nice_words
+    else
+      puts "error, expecting either basic or improved"
     nice_words
+    end
   end
 
   private
@@ -37,6 +46,14 @@ class CheckNiceWords
     @nice_words
   end
 
+  def count_improved_nice_words()
+    @nice_words = 0
+    words.each do |word|
+      @nice_words += 1 if word.improved_nice?
+    end
+    @nice_words
+  end
+
 end
 
 class String
@@ -51,4 +68,9 @@ class String
     no_bad_strings = not_ab && not_cd && not_pq && not_xy
     !!(three_vowels && repeated_chars &&(no_bad_strings))
   end
+
+  def improved_nice?
+    !!(self =~ CheckNiceWords::PAIR_TWICE && self =~ CheckNiceWords::BOB)
+  end
+
 end
