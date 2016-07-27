fc7d636e2b85f6eb41509563bc843b8c722f22ae
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-07-27 20:25:11 +0100

    add day7 WIP code

--- day7/lib/day7.rb
+++ day7/lib/day7.rb
@@ -1,22 +1,36 @@
 class Day7
 
-  attr_reader :lines
+  attr_accessor :lines
 
-  def get_raw_data
+  def initialize
     data = File.read("lib/input.txt")
     @lines = data.split("\n")
   end
 
   def reverse_data
-    @lines.map do |line|
-      reverse(line)
-    end
-    lines
+    @lines.map! { |line| reverse(line) }
   end
 
   def reverse(line)
     line.split("->").reverse
   end
 
+  def sort_file
+    lines.sort_by!{|e| [e[0].length, e[0]]}
 
+  end
+
+  def trim
+    lines.map!{ |e| [e[0].strip, e[1].strip] }
+  end
+
+  def convert_to_hash
+     @lines = lines.to_h
+  end
+
+  def process_signals
+    result_key, result_value = lines.first
+
+
+  end
 end
--- day7/spec/day7_spec.rb
+++ day7/spec/day7_spec.rb
@@ -1,7 +1,7 @@
 require 'day7'
 
 describe Day7 do
-  subject(:d7) { Day7.new }
+  subject(:d7) { described_class.new }
   before(:each) do
     @f = File.read("lib/input.txt")
     @lines = @f.split("\n")
@@ -17,11 +17,11 @@ describe Day7 do
     end
 
     it "splits file in lines" do
-      expect(d7.get_raw_data.count).to eq 339
+      expect(d7.lines.count).to eq 339
     end
 
     it "check last line of the file's content" do
-      expect(d7.get_raw_data[-1]).to eq @result
+      expect(d7.lines[-1]).to eq @result
     end
   end
 
@@ -29,6 +29,8 @@ describe Day7 do
     before do
       @line = "lf AND lq -> ls"
       @reverse = [" ls", "lf AND lq "]
+      @rev_last_line = [" hh", "he RSHIFT 5 "]
+      @first_sorted_item = [" a", "lx "]
     end
 
     it "reverses one line" do
@@ -36,7 +38,27 @@ describe Day7 do
     end
 
     it "reverses file" do
-
+      expect(d7.reverse_data[-1]).to eq @rev_last_line
     end
+
+    it "sorts file by first element" do
+      d7.reverse_data
+      expect(d7.sort_file[0]).to eq @first_sorted_item
+    end
+
+    it "trims properly the file" do
+      d7.reverse_data
+      d7.trim
+      expect(d7.lines[-1]).to eq ["hh", "he RSHIFT 5"]
+    end
+
+    it "converts to hash" do
+      d7.reverse_data
+      d7.convert_to_hash
+      expect(d7.lines).to be_instance_of(Hash)
+      expect(d7.lines.first).to eq [" ls", "lf AND lq "]
+    end
+
+
   end
 end
